00ab5d063a8d80085cf561c801941b912727faa7
pransh1 <mauryapransh2@gmail.com>
2024-11-24 23:59:00 +0530

    made the react file for login and sign up

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "../styles/SignIn.css";
 
 const SignIn = () => {
+
+  const [formData, setFormData] = useState({
+    email:"",
+    password:"",
+  });
+
+  const [error, setError] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+
+    setError("");
+    setSuccessMessage("");
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("/api/login", formData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if(response.status === 200) {
+        setSuccessMessage("Login Successfully");
+      }
+      localStorage.setItem("token", response.data.token);
+
+      window.location.href = "/";
+    }
+    catch(error) {
+      if(error.response) {
+        setError(error.response.data.message || "Login Failed");
+      }
+      else {
+        setError("An error occured, Please try again");
+      }
+    }
+  };
+  
   return (
     <>
       <div class="wrapper sign-in">
@@ -11,32 +58,35 @@ const SignIn = () => {
           </span>
         </a>
         <h2>Sign In</h2>
-
-        <div class="input-box">
-          <span class="icon"><i class='bx bx-envelope'></i></span>
-          <input type="email" required />
-            <label>Email</label>
-        </div>
-        <div class="input-box">
-          <span class="icon"><ion-icon name="call-outline"></ion-icon></span>
-          <input type="tel" required />
-            <label>Phone Number</label>
-        </div>
-        <div class="remember-forgot">
-          <label><input type="checkbox" />Remember me</label>
-        </div>
-        <a href="dashboard.html">
-          <button type="submit" className="btn">
-            Sign In
-          </button>
-        </a>
-
-        <div class="login-register">
-          <p>Don't have a account?<a href="" class="register-link">
-            <button class="btn">Sign Up</button>
+        {error && <p className="error-message">{error}</p>}
+        {successMessage && <p className="success-message">{successMessage}</p>}
+        <form onSubmit={handleSubmit}>
+          <div class="input-box">
+            <span class="icon"><i class='bx bx-envelope'></i></span>
+            <input type="email" name="email" placeholder="Enter your email" required value={formData.email} onChange={handleChange}/>
+              <label>Email</label>
+          </div>
+          <div class="input-box">
+            <span class="icon"><ion-icon name="lock-closed-outline"></ion-icon></span>
+            <input type="password" name="password" placeholder="Enter your password" required value={formData.password} onChange={handleChange}/>
+              <label>Phone Number</label>
+          </div>
+          <div class="remember-forgot">
+            <label><input type="checkbox" />Remember me</label>
+          </div>
+          <a href="dashboard.html">
+            <button type="submit" className="btn">
+              Sign In
+            </button>
           </a>
-          </p>
-        </div>
+
+          <div class="login-register">
+            <p>Don't have a account?<a href="/signup" class="register-link">
+              <button class="btn">Sign Up</button>
+            </a>
+            </p>
+          </div>
+        </form>  
       </div>
     </>
   );
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,61 +1,179 @@
 import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import "../styles/SignUp.css";
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+    role: "",
+  });
 
+  const [error, setError] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const handleChange = async (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+    setError("");
+    setSuccessMessage("");
+  };
 
-  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!formData.role) {
+      setError("please select a role");
+    }
+
+    try {
+      const response = await axios.post("/api/register", formData, {
+        headers: {
+          "Content-type": "application/json",
+        },
+      });
+
+      if (response.status === 201) {
+        setSuccessMessage("Registered Successfully");
+        setFormData({
+          username: "",
+          email: "",
+          phoneNumber: "",
+          password: "",
+          role: "",
+        });
+      }
+    } catch (error) {
+      if (error.response) {
+        setError(error.response.data.message || "Registeration failed");
+      } else {
+        setError("An error occured. Please try again");
+      }
+    }
+  };
+
   return (
     <>
       <div class="wrapper register">
-        <a href="index.html"><span class="icon-close"><ion-icon name="close-outline"></ion-icon></span></a>
+        <a href="index.html">
+          <span class="icon-close">
+            <ion-icon name="close-outline"></ion-icon>
+          </span>
+        </a>
 
         <h2>Registration</h2>
+        {error && <p class="error-message">{error}</p>}
+        {successMessage && <p class="success-message">{successMessage}</p>}
 
-        <div class="input-box">
-          <span class="icon"><i class='bx bx-user'></i></span>
-          <input type="text" required />
+        <form onSubmit={handleSubmit}>
+          <div class="input-box">
+            <span class="icon">
+              <i class="bx bx-user"></i>
+            </span>
+            <input
+              type="text"
+              name="username"
+              placeholder="username"
+              required
+              value={formData.username}
+              onChange={handleChange}
+            />
             <label>Username</label>
-        </div>
-
-        <div class="input-box">
-          <span class="icon"><i class='bx bx-envelope'></i></span>
-          <input type="email" required />
+          </div>
+          <div class="input-box">
+            <span class="icon">
+              <i class="bx bx-envelope"></i>
+            </span>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              required
+              value={formData.email}
+              onChange={handleChange}
+            />
             <label>Email</label>
-        </div>
-        <div class="input-box">
-          <span class="icon"><ion-icon name="call-outline"></ion-icon></span>
-          <input type="tel" required />
+          </div>
+          <div class="input-box">
+            <span class="icon">
+              <ion-icon name="call-outline"></ion-icon>
+            </span>
+            <input
+              type="tel"
+              name="phoneNumber"
+              placeholder="phone Number"
+              required
+              value={formData.phoneNumber}
+              onChange={handleChange}
+            />
             <label>Phone Number</label>
-        </div>
-        <div class="input-box">
-          <span class="icon"><ion-icon name="lock-closed-outline"></ion-icon></span>
-          <input type="password" required />
+          </div>
+          <div class="input-box">
+            <span class="icon">
+              <ion-icon name="lock-closed-outline"></ion-icon>
+            </span>
+            <input
+              type="password"
+              name="password"
+              placeholder="password"
+              required
+              value={formData.password}
+              onChange={handleChange}
+            />
             <label>Password</label>
-        </div>
-        <div className="role-selection">
-          <h3>Select Role</h3>
-          <label>
-            <input type="radio" name="role" value="user" /> User
-          </label>
-          <label>
-            <input type="radio" name="role" value="admin" /> Admin
-          </label>
-        </div>
-        <br/>
-        <div class="remember-forgot">
-          <label><input type="checkbox" />Remember me</label>
-        </div>
-        <a href="login.html"><button type="submit" class="btn">Register</button></a>
-        <div class="login-register">
-          <p>Already have an account?<a href="" class="register-link"><button class="btn">Sign In</button>
-
+          </div>
+          <div className="role-selection">
+            <h3>Select Role</h3>
+            <label>
+              <input
+                type="radio"
+                name="role"
+                value="user"
+                onChange={handleChange}
+                checked={formData.role === "user"}
+              />
+              User
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="role"
+                value="admin"
+                onChange={handleChange}
+                checked={formData.role === "admin"}
+              />
+              Admin
+            </label>
+          </div>
+          <br />
+          <div class="remember-forgot">
+            <label>
+              <input type="checkbox" />
+              Remember me
+            </label>
+          </div>
+          <a href="login.html">
+            <button type="submit" class="btn">
+              Register
+            </button>
           </a>
-          </p>
-        </div>
+          <div class="login-register">
+            <p>
+              Already have an account?
+              <a href="/signin" class="register-link">
+                <button class="btn">Sign In</button>
+              </a>
+            </p>
+          </div>
+        </form>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default SignUp;
